427e1027f4dc51ca7074daa54630328052e6392f
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-08 09:35:37 +0100

    Remove console logs

--- event/app/components/DevLogin.jsx
+++ event/app/components/DevLogin.jsx
@@ -5,13 +5,8 @@ import DevLoginForm from "./DevLoginForm";
 export default async function DevLogin({ children }) {
 
     const cookieStore = await cookies();
-    console.log(cookieStore);
     const password = cookieStore.get("password");
-    console.log("env", process.env.PASSWORD);
-    console.log("cookie PW", password);
     const isLoggedIn = password && password.value === process.env.PASSWORD;
-    console.log("logged in:", isLoggedIn);
-
 
     return <>{isLoggedIn ? children : <DevLoginForm />}</>;
 }
