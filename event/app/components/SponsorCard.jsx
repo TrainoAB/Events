5b87f5df39b2d1fc52aceec1d201f488242405a3
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-11 14:34:39 +0100

    Sponsor page markup.

--- event/app/components/SponsorCard.jsx
+++ event/app/components/SponsorCard.jsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 import "./SponsorCard.css";
 
-export default function SponsorCard() {
-  return (
-    <div>SponsorCard</div>
-  )
+export default function SponsorCard({ image, title, description }) {
+    return (
+        <article className="sponsor-card box-shadow b-radius">
+            <Image src={image} width={100} height={100} alt={`${title} sponsor image`} />
+            <h3 className="sponsor-card__title">{title}</h3>
+            <p className="sponsor-card__description">{description}</p>
+        </article>
+    );
 }
