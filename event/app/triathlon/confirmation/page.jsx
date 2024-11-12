9e5dbb5367a405698fdebbd4726f35a47e92261f
joelBit <joel.rollny@gmail.com>
2024-11-12 09:44:04 +0100

    Created confirmation page

--- event/app/triathlon/confirmation/page.jsx
+++ event/app/triathlon/confirmation/page.jsx
@@ -1,9 +1,32 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import "./page.css";
 
 export default function ConfirmationPage() {
+    const router = useRouter();
+
+    const handleNavigate = () => {
+        router.push('/triathlon/participants');
+    };
+
     return (
-        <main id="confirmationpage">
-            <h1>Bekräftelse</h1>
+        <main id="confirmationpage" className="gap">
+            <figure className='box-shadow '>
+                <img src={'/checkmark.png'} alt="confirmation checkmark" />
+            </figure>
+
+            <div>
+                <h3>Tack!</h3>
+                <p className='max-width'>
+                    Du har nu blivit anmäld till tävlingen. Du kommer att
+                    få en bekräftelse skickad till den angivna mailen.
+                </p>
+            </div>
+            
+            <button onClick={handleNavigate}>
+                Visa deltagare
+            </button>
         </main>
     );
 }
