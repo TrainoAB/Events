7bcfa30afdd0c68a25f741834258d0fac49409c2
joelBit <joel.rollny@gmail.com>
2024-11-12 13:26:02 +0100

    Created FAQ page

--- event/app/triathlon/faq/page.jsx
+++ event/app/triathlon/faq/page.jsx
@@ -1,9 +1,22 @@
+import Accordion from "@/app/components/Accordion";
 import "./page.css";
 
 export default function FAQPage() {
+    
+    // Temporary questions and answers
+    const FAQ = [{question: "Hur går jag tillväga för att ...?", answer: "Du går till höger."},
+        {question: "Får jag pengarna tillbaka?", answer: "Nej, inga återbetalningar sker."},
+        {question: "Hur laddar man ner något?", answer: "Du går dit och sen trycker du där."},
+        {question: "Finns det en möjlighet att göra detta?", answer: "Lorem ipsum dolor sit amet consectetur. Enim vitae nullam id tempor quis quis. Proin risus id hendrerit neque tristique. Aenean mus sagittis ornare sem morbi tellus. Ultrices ultrices tempus consectetur nulla morbi dui viverra. Commodo justo viverra neque tincidunt tincidunt pellentesque malesuada ac. Egestas ultricies adipiscing ac proin volutpat tincidunt ac."}
+    ];
+
     return (
-        <main id="faqpage">
-            <h1>F.A.Q</h1>
+        <main id="faqpage" className="gap">
+            <h1 className="faq__title">FAQ</h1>
+
+            {FAQ.map(faq => 
+                <Accordion heading={faq.question} text={faq.answer} />
+            )}
         </main>
     );
 }
