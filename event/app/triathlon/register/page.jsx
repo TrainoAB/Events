7aa079130983a53f939fb97097b3a36056861975
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-12 10:00:47 +0100

    Add initial markup for register page.

--- event/app/triathlon/register/page.jsx
+++ event/app/triathlon/register/page.jsx
@@ -3,7 +3,59 @@ import "./page.css";
 export default function RegisterPage() {
     return (
         <main id="registerpage">
-            <h1>Registrera mig</h1>
+            <h1>Registrera</h1>
+            <form className="register-form">
+                <div className="input-wrapper">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" type="email" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="firstname">Förnamn</label>
+                    <input id="firstname" name="firstname" type="text" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="lastname">Efternamn</label>
+                    <input id="lastname" name="lastname" type="text" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="age">Ålder</label>
+                    <input id="age" name="age" type="text" />
+                </div>
+                <div className="input-wrapper">
+                    <span classname="gender-label">Kön</span>
+                    <div className="radio-wrapper">
+                        <input id="man" name="gender" type="radio" value="man" />
+                        <label htmlFor="man">Man</label>
+                        <input id="woman" name="gender" type="radio" value="woman" />
+                        <label htmlFor="woman">Kvinna</label>
+                    </div>
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="phonenumber">Telefonnummer</label>
+                    <input id="phonenumber" name="phonenumber" type="tel" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="city">Stad</label>
+                    <input id="city" name="city" type="text" />
+                </div>
+                <div className="input-wrapper">
+                    <span classname="gender-label">Tävling</span>
+                    <div className="radio-wrapper">
+                        <input id="triathlon" name="competition" type="radio" value="triathlon" />
+                        <label htmlFor="triathlon">Triathlon</label>
+                        <input
+                            id="olympic-triathlon"
+                            name="competition"
+                            type="radio"
+                            value="olympic-triathlon"
+                        />
+                        <label htmlFor="olympic-triathlon">Olympiskt Triathlon</label>
+                    </div>
+                </div>
+                <button className="register-form__submit" type="submit">
+                    Registrera mig
+                </button>
+            </form>
         </main>
     );
 }
