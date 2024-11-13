45915cce07545927513148c4def32c445ae4afb2
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-13 10:02:22 +0100

    Add initial markup and CSS for about page and ImageGallery component.

--- event/next.config.mjs
+++ event/next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "picsum.photos",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "traino.s3.eu-north-1.amazonaws.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "traino.nu",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
