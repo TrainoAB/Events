18dec5933feb5aa5564567c04941070469a47cf1
joelBit <joel.rollny@gmail.com>
2024-11-13 10:17:29 +0100

    Created test data

--- event/app/triathlon/participants/page.jsx
+++ event/app/triathlon/participants/page.jsx
@@ -1,9 +1,25 @@
 import "./page.css";
 
 export default function ParticipantsPage() {
+
+    const PRODUCTS = [
+        {name: "Greger Artursson", city: "Luleå", competition: "triathlon"},
+        {name: "Pelle Jöns", city: "Stockholm", competition: "olympic"},
+        {name: "Sonja Andersson", city: "Växjö", competition: "triathlon"},
+        {name: "Arne Björnsson", city: "Karlstad", competition: "olympic"},
+        {name: "Karl Bengtsson", city: "Halmstad", competition: "triathlon"},
+        {name: "Sara Viktorsson", city: "Göteborg", competition: "olympic"},
+        {name: "Jane Doe", city: "Stockholm", competition: "triathlon"},
+        {name: "John Doe", city: "Uppsala", competition: "triathlon"},
+        {name: "Göran Petterson", city: "Malmö", competition: "olympic"},
+        {name: "Lisa Tjäderstig", city: "Jönköping", competition: "olympic"}
+    ];
+
     return (
-        <main id="participantspage">
-            <h1>Deltagare</h1>
+        <main id="participantspage" className="gap">
+            <h1 className="participants__title">Deltagare</h1>
+
+
         </main>
     );
 }
