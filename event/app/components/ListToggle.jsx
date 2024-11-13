daf4317f7fe097526c183608c4c5304b58a584e4
joelBit <joel.rollny@gmail.com>
2024-11-13 14:13:33 +0100

    Created Toggle component and participants page

--- event/app/components/ListToggle.jsx
+++ event/app/components/ListToggle.jsx
@@ -1,7 +1,32 @@
+'use client';
+
+import { useState } from "react";
 import "./ListToggle.css";
 
-export default function ListToggle() {
-  return (
-    <div>ListToggle</div>
-  )
+export default function ListToggle({ title1, list1, title2, list2 }) {
+    const [isShowFirst, setShowFirst] = useState(true);
+
+    const handleToggle = (event) => {
+        if (event.target.className.includes('title2') && !isShowFirst || event.target.className.includes('title1') && isShowFirst) {
+            return;   // If the clicked title is already active, do nothing
+        }
+
+        setShowFirst(!isShowFirst);
+    };
+
+    return (
+        <section className="list-toggle max-width">
+            <div className="list-toggle__titles">
+                <h2 onClick={handleToggle} 
+                    className={isShowFirst ? 'list-toggle__title1 list-toggle__title--active' : 'list-toggle__title1'}>
+                    {title1}
+                </h2>
+                <h2 onClick={handleToggle} 
+                    className={!isShowFirst ? 'list-toggle__title2 list-toggle__title--active' : 'list-toggle__title2'}>
+                    {title2}
+                </h2>
+            </div>
+            { isShowFirst ? list1 : list2 }
+        </section>
+    )
 }
--- event/app/triathlon/participants/page.jsx
+++ event/app/triathlon/participants/page.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+import ListToggle from "@/app/components/ListToggle";
+
 import "./page.css";
 
 export default function ParticipantsPage() {
-
-    const PRODUCTS = [
+    
+    // Temporary Participants
+    const PARTICIPANTS = [
         {name: "Greger Artursson", city: "Luleå", competition: "triathlon"},
         {name: "Pelle Jöns", city: "Stockholm", competition: "olympic"},
         {name: "Sonja Andersson", city: "Växjö", competition: "triathlon"},
@@ -15,11 +19,46 @@ export default function ParticipantsPage() {
         {name: "Lisa Tjäderstig", city: "Jönköping", competition: "olympic"}
     ];
 
+    const createList = (competition) => {
+        return (
+            <section>
+                <div className="participants-list__heading box-shadow">
+                    <h3 className="participants-list__heading-participants">Deltagare</h3> 
+                    <h3 className="participants-list__heading-city">Stad</h3>
+                </div>
+                { PARTICIPANTS
+                .filter(el => el.competition === competition)
+                .map(el => <div className="participants-list__row box-shadow">
+                                <p className="participants-list__name">{el.name}</p> 
+                                <p className="participants-list__city">{el.city}</p>
+                            </div>)
+                }
+            </section>
+        );
+    }
+
     return (
         <main id="participantspage" className="gap">
             <h1 className="participants__title">Deltagare</h1>
+            <h2 className="participants-numbers">
+                <div className="participants__confirmed">{PARTICIPANTS.length}</div>
+                /100
+            </h2>
+            <ListToggle 
+                title1="Triathlon" 
+                list1={createList("triathlon")} 
+                title2="Olympiskt triathlon" 
+                list2={createList("olympic")}
+            />
 
-
+            <div className="traino-funnel">
+                <p className="traino-funnel__text">
+                    Taggad på att träna efter du sett motståndet?
+                </p>
+                <Link href="https://traino.nu/" className="box-shadow link-btn">
+                    Träna
+                </Link>
+            </div>
         </main>
     );
 }
