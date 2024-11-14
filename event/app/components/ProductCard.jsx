4bf937791f299a8ff5a5643d41ac9b2de4d526e7
joelBit <joel.rollny@gmail.com>
2024-11-14 10:14:15 +0100

    Use Image instead of img

--- event/app/components/ProductCard.jsx
+++ event/app/components/ProductCard.jsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
+
 import "./ProductCard.css";
 
 export default function ProductCard({ image, url }) {
     return (
         <section className="product-card">
-            <img src={image} />
+            <Image
+                src={image}
+                width={272}
+                height={484}
+            />
             <a href={url} target="_blank" className="product-card__button box-shadow">Gå till merch</a>
         </section>
     );
