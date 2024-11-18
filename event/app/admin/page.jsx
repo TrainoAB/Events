5474fcd71077d544e0bcd3529630c69c8cf1364e
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-18 13:43:50 +0100

    Add markup for admin page.

--- event/app/admin/page.jsx
+++ event/app/admin/page.jsx
@@ -1,5 +1,38 @@
+import EventCard from "../components/EventCard";
+
 import "./page.css";
 
+const EVENTS = [
+    {
+        url: "/admin/manage-event",
+        image: "https://picsum.photos/484/272",
+        date: "16 Aug. 2024",
+        competition: "Triathlon",
+        description: "Stockholm. Även ett mini olympiskt triathlon.",
+    },
+    {
+        url: "#",
+        image: "https://picsum.photos/484/272",
+        date: "N/A",
+        competition: "E-Sport Challenge",
+        description: "Detta event planeras fortfarande.",
+    },
+];
+
 export default function AdminPage() {
-    return <div>Admin Page</div>;
+    return (
+        <main id="admin-page" className="max-width gap">
+            <h1 className="admin-page__title">Manage Events</h1>
+            <div className="events">
+                {EVENTS.map((event, i) => (
+                    <div className="event-wrapper" key={i} >
+                        <EventCard event={event} />
+                        <button className="delete-event-btn">Radera</button>
+                    </div>
+                ))}
+            </div>
+            {/* TODO: Push/link to an add event form */}
+            <button className="add-event-btn"></button>
+        </main>
+    );
 }
