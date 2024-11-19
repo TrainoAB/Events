0903dcd36ebd91935c5533526d2af6ba3dc5a700
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-19 11:10:53 +0100

    Add initial markup for edit event page.

--- event/app/admin/(manage-event)/[id]/event/page.jsx
+++ event/app/admin/(manage-event)/[id]/event/page.jsx
@@ -1,8 +1,28 @@
+import "./page.css";
 
-export default function EditEventPage() {
-  return (
-    <main id="edit-event-page">
-        <div>Edit Event</div>
-    </main>
-  )
+export default function EditEventPage({ params }) {
+    return (
+        <main id="edit-event-page">
+            <h1 className="edit-event-page__title">{`Editing ${params.id}`}</h1>
+            <form className="edit-event-form">
+                <div className="input-wrapper">
+                    <label htmlFor="event">Event</label>
+                    <input id="event" name="event" type="text" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="date">Datum</label>
+                    <input id="date" name="date" type="date" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="description">Beskrivning</label>
+                    <textarea id="description" name="description" rows={10} cols={100} />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="image">Bild (text länk)</label>
+                    <input id="image" name="image" type="text" />
+                </div>
+                <button className="edit-event-form__submit">Lägg till</button>
+            </form>
+        </main>
+    );
 }
