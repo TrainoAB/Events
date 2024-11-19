ccd77f6342fff49b2b2f69bbe10648a198a8c35d
joelBit <joel.rollny@gmail.com>
2024-11-19 11:48:28 +0100

    Created Manage Sponsor admin page

--- event/app/admin/(manage-event)/[id]/sponsors/page.jsx
+++ event/app/admin/(manage-event)/[id]/sponsors/page.jsx
@@ -1,7 +1,53 @@
+import SponsorCard from "@/app/components/SponsorCard";
+
+import "./page.css";
+
 export default function ManageSponsorsPage() {
-  return (
-    <main id="manage-sponsors-page">
-        <div>Manage Sponsors</div>
-    </main>
-  )
+    return (
+        <main id="manage-sponsors-page" className="gap">
+            <h1 className="manage-sponsors__title">Hantera sponsorer</h1>
+
+            <section className="sponsors max-width">
+                {SPONSORS.map((sponsor, index) => (
+                    <div className="sponsor-wrapper" key={index} >
+                        <SponsorCard
+                            image={sponsor.image}
+                            title={sponsor.name}
+                            description={sponsor.description}
+                        />
+                        <button className="delete-sponsor-btn">Radera</button>
+                    </div>
+                ))}
+            </section>
+
+            <button className="add-sponsor-btn"></button>
+        </main>
+    )
 }
+
+const SPONSORS = [
+  {
+      name: "McDonald's",
+      description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus ex, omnis nulla similique itaque reprehenderit, ut nemo hic cupiditate molestias voluptatem necessitatibus quibusdam ea incidunt ipsum eveniet maiores adipisci assumenda numquam! Dolore quasi exercitationem saepe aperiam, id ut. Ullam alias obcaecati eum error dignissimos commodi excepturi qui nam cumque?",
+      image: "https://picsum.photos/200"
+  },
+  {
+      name: "Lamborghini",
+      description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus ex, omnis nulla similique itaque reprehenderit, ut nemo hic cupiditate molestias voluptatem necessitatibus quibusdam ea incidunt ipsum eveniet maiores adipisci assumenda numquam! Dolore quasi exercitationem saepe aperiam, id ut. Ullam alias obcaecati eum error dignissimos commodi excepturi qui nam cumque?",
+      image: "https://picsum.photos/200"
+  },
+  {
+    name: "Pizza Hut",
+    description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus ex, omnis nulla similique itaque reprehenderit, ut nemo hic cupiditate molestias voluptatem necessitatibus quibusdam ea incidunt ipsum eveniet maiores adipisci assumenda numquam! Dolore quasi exercitationem saepe aperiam, id ut. Ullam alias obcaecati eum error dignissimos commodi excepturi qui nam cumque?",
+    image: "https://picsum.photos/200"
+},
+{
+    name: "Tesla",
+    description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus ex, omnis nulla similique itaque reprehenderit, ut nemo hic cupiditate molestias voluptatem necessitatibus quibusdam ea incidunt ipsum eveniet maiores adipisci assumenda numquam! Dolore quasi exercitationem saepe aperiam, id ut. Ullam alias obcaecati eum error dignissimos commodi excepturi qui nam cumque?",
+    image: "https://picsum.photos/200"
+}
+];
