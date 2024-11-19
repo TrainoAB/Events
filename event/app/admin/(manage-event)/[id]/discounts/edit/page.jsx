b84b2a7ceb478e0cdfa63d282f99173ca5ac433a
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-19 14:07:54 +0100

    Add initial markup for edit discount page.

--- event/app/admin/(manage-event)/[id]/discounts/edit/page.jsx
+++ event/app/admin/(manage-event)/[id]/discounts/edit/page.jsx
@@ -1,8 +1,28 @@
+import "./page.css";
 
 export default function EditDiscountPage() {
-  return (
-    <main id="edit-discount-page">
-        Edit discount
-    </main>
-  )
+    return (
+        <main id="edit-discount-page">
+            <h1 className="edit-discount-page__title">Redigera Rabatt</h1>
+            <form className="edit-discount-form">
+                <div className="input-wrapper">
+                    <label htmlFor="title">Rubkrik</label>
+                    <input id="title" name="title" type="text" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="discount">Rabatt</label>
+                    <input id="discount" name="discount" type="number" step={1} min={1} max={100} />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="link">Länk</label>
+                    <input id="link" name="link" type="text" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="description">Beskrivning</label>
+                    <textarea id="description" name="description" rows={10} cols={100} />
+                </div>
+                <button className="edit-discount-form__submit">Spara</button>
+            </form>
+        </main>
+    );
 }
