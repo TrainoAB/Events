714a2d8116fcee478d5af5c3f9cfe1ebee80754a
joelBit <joel.rollny@gmail.com>
2024-11-20 12:55:18 +0100

    modal on admin discount page

--- event/app/admin/(manage-event)/[id]/discounts/page.jsx
+++ event/app/admin/(manage-event)/[id]/discounts/page.jsx
@@ -1,5 +1,9 @@
+'use client';
+
+import { useState } from "react";
 import Link from "next/link";
 import DiscountCard from "@/app/components/DiscountCard";
+import { Modal } from "@/app/components/Modal";
 
 import "./page.css";
 
@@ -19,6 +23,22 @@ const DISCOUNTS = [
 ];
 
 export default function ManageDiscountsPage({ params }) {
+    const [ showModal, setShowModal ] = useState(false);
+    const [ discount, setDiscount ] = useState();
+
+    const handleDeleteClick = (discount) => {
+        setDiscount(discount);
+        setShowModal(!showModal);
+    }
+
+    const handleDeleteDiscount = () => {
+        console.log(`delete ${discount.title}`);
+    }
+
+    const handleToggleModal = () => {
+        setShowModal(!showModal);
+    }
+
     return (
         <main id="manage-discounts-page">
             <h1 className="manage-discounts-page__title">{`Rabatter: ${params.id}`}</h1>
@@ -28,10 +48,17 @@ export default function ManageDiscountsPage({ params }) {
                         <Link href="discounts/edit">
                             <DiscountCard discount={discount} />
                         </Link>
-                        <button className="delete-btn">Radera</button>
+                        <button className="delete-btn" onClick={() => handleDeleteClick(discount)}> Radera </button>
                     </div>
                 ))}
             </div>
+
+            {showModal ? <Modal 
+                            title={discount.title}
+                            closeModal={handleToggleModal} 
+                            confirm={handleDeleteDiscount} 
+                        /> : <></> 
+            }
 
             <Link href="/admin/1/discounts/add" className="manage-discounts__add-link">
                 <button className="add-btn"></button>
