98bed26f6dbd88f92548337dfc6a952452026eb5
joelBit <joel.rollny@gmail.com>
2024-11-21 13:52:57 +0100

    highlight active link in AdminHeader

--- event/app/components/AdminHeader.jsx
+++ event/app/components/AdminHeader.jsx
@@ -1,22 +1,26 @@
-import Link from "next/link";
+'use client'
+ 
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 import "./AdminHeader.css";
 
 export default function AdminHeader() {
+    const pathname = usePathname();
 
     return (
         <nav id="admin-header">
             <ul className="admin-header-list">
-                <li className="admin-header-list__element">
+                <li className='admin-header-list__element'>
                     <Link href="/admin">Alla Event</Link>
                 </li>
-                <li className="admin-header-list__element">
+                <li className={`${pathname === '/admin/1/event' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
                     <Link href="/admin/1/event">Redigera Eventet</Link>
                 </li>
-                <li className="admin-header-list__element">
+                <li className={`${pathname === '/admin/1/discounts' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
                     <Link href="/admin/1/discounts">Rabatter</Link>
                 </li>
-                <li className="admin-header-list__element">
+                <li className={`${pathname === '/admin/1/sponsors' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
                     <Link href="/admin/1/sponsors">Sponsorer</Link>
                 </li>
             </ul>
