c7601046166ba5f9f124a3bd8d05668820f2e701
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-21 10:55:06 +0100

    Wrap SponsorCard in a Link. Add hover effect.

--- event/app/admin/(manage-event)/[id]/sponsors/page.jsx
+++ event/app/admin/(manage-event)/[id]/sponsors/page.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import Link from "next/link";
@@ -8,21 +8,21 @@ import { Modal } from "@/app/components/Modal";
 import "./page.css";
 
 export default function ManageSponsorsPage({ params }) {
-    const [ showModal, setShowModal ] = useState(false);
-    const [ sponsor, setSponsor ] = useState();
+    const [showModal, setShowModal] = useState(false);
+    const [sponsor, setSponsor] = useState();
 
     const handleDeleteClick = (sponsor) => {
         setSponsor(sponsor);
         setShowModal(!showModal);
-    }
+    };
 
     const handleDeleteSponsor = () => {
         console.log(`delete ${sponsor.name}`);
-    }
+    };
 
     const handleToggleModal = () => {
         setShowModal(!showModal);
-    }
+    };
 
     return (
         <main id="manage-sponsors-page" className="gap">
@@ -31,24 +31,23 @@ export default function ManageSponsorsPage({ params }) {
             <section className="sponsors max-width">
                 {SPONSORS.map((sponsor, index) => (
                     <div className="sponsor-wrapper" key={index}>
-                        <Link href="sponsors/edit">
-                            <SponsorCard
-                                image={sponsor.image}
-                                title={sponsor.name}
-                                description={sponsor.description}
-                            />
-                        </Link>
-                        <button className="delete-btn" onClick={() => handleDeleteClick(sponsor)}> Radera </button>
+                        <SponsorCard sponsor={sponsor} external={false} />
+                        <button className="delete-btn" onClick={() => handleDeleteClick(sponsor)}>
+                            Radera
+                        </button>
                     </div>
                 ))}
             </section>
 
-            {showModal ? <Modal 
-                            title={sponsor.name}
-                            closeModal={handleToggleModal} 
-                            confirm={handleDeleteSponsor} 
-                        /> : <></> 
-            }
+            {showModal ? (
+                <Modal
+                    title={sponsor.name}
+                    closeModal={handleToggleModal}
+                    confirm={handleDeleteSponsor}
+                />
+            ) : (
+                <></>
+            )}
 
             <Link href="/admin/1/sponsors/add" className="manage-sponsors__add-link">
                 <button className="add-btn"></button>
@@ -59,24 +58,28 @@ export default function ManageSponsorsPage({ params }) {
 
 const SPONSORS = [
     {
+        url: "sponsors/edit",
         name: "McDonald's",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus ex, omnis nulla similique itaque reprehenderit, ut nemo hic cupiditate molestias voluptatem necessitatibus quibusdam ea incidunt ipsum eveniet maiores adipisci assumenda numquam! Dolore quasi exercitationem saepe aperiam, id ut. Ullam alias obcaecati eum error dignissimos commodi excepturi qui nam cumque?",
         image: "https://picsum.photos/200",
     },
     {
+        url: "sponsors/edit",
         name: "Lamborghini",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus ex, omnis nulla similique itaque reprehenderit, ut nemo hic cupiditate molestias voluptatem necessitatibus quibusdam ea incidunt ipsum eveniet maiores adipisci assumenda numquam! Dolore quasi exercitationem saepe aperiam, id ut. Ullam alias obcaecati eum error dignissimos commodi excepturi qui nam cumque?",
         image: "https://picsum.photos/200",
     },
     {
+        url: "sponsors/edit",
         name: "Pizza Hut",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus ex, omnis nulla similique itaque reprehenderit, ut nemo hic cupiditate molestias voluptatem necessitatibus quibusdam ea incidunt ipsum eveniet maiores adipisci assumenda numquam! Dolore quasi exercitationem saepe aperiam, id ut. Ullam alias obcaecati eum error dignissimos commodi excepturi qui nam cumque?",
         image: "https://picsum.photos/200",
     },
     {
+        url: "sponsors/edit",
         name: "Tesla",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus doloribus ex, omnis nulla similique itaque reprehenderit, ut nemo hic cupiditate molestias voluptatem necessitatibus quibusdam ea incidunt ipsum eveniet maiores adipisci assumenda numquam! Dolore quasi exercitationem saepe aperiam, id ut. Ullam alias obcaecati eum error dignissimos commodi excepturi qui nam cumque?",
--- event/app/components/SponsorCard.jsx
+++ event/app/components/SponsorCard.jsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import "./SponsorCard.css";
 
-export default function SponsorCard({ image, title, description }) {
+export default function SponsorCard({ sponsor, external }) {
     return (
-        <article className="sponsor-card box-shadow b-radius">
-            <figure className="sponsor-card__figure">
-                <Image src={image} width={200} height={200} alt={`${title} sponsor image`} />
-            </figure>
-            <div className="sponsor-card-wrapper">
-                <h3 className="sponsor-card__title">{title}</h3>
-                <p className="sponsor-card__description">{description}</p>
-            </div>
-        </article>
+        <Link className="sponsor-card-link" href={sponsor.url} target={external ? "_blank" : ""}>
+            <article className="sponsor-card box-shadow b-radius">
+                <figure className="sponsor-card__figure">
+                    <Image
+                        src={sponsor.image}
+                        width={200}
+                        height={200}
+                        alt={`${sponsor.name} sponsor image`}
+                    />
+                </figure>
+                <div className="sponsor-card-wrapper">
+                    <h3 className="sponsor-card__title">{sponsor.name}</h3>
+                    <p className="sponsor-card__description">{sponsor.description}</p>
+                </div>
+            </article>
+        </Link>
     );
 }
