72a3572377e73ed9a6f929e118d2bf61478ef95f
joelBit <joel.rollny@gmail.com>
2024-11-26 13:24:40 +0100

    Fixed AdminHeader mobile menu

--- event/app/components/AdminHeader.jsx
+++ event/app/components/AdminHeader.jsx
@@ -3,41 +3,40 @@
 import { useState } from "react";
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import Image from "next/image";
 
 import "./AdminHeader.css";
 
 export default function AdminHeader() {
-    const [showMenu, setShowMenu] = useState(false);
+    const [ showMenu, setShowMenu ] = useState(false);
     const pathname = usePathname();
 
-    const handleOpenMenu = () => {
-        setShowMenu(true);
-    };
-
-    const handleCloseMenu = () => {
-        setShowMenu(false);
-    };
+    const handleShowMenu = () => {
+        setShowMenu(!showMenu);
+    }
 
     return (
-        <nav id="admin-header">
-            <div className="icon-menu" onClick={handleOpenMenu}></div>
-            <ul className={showMenu ? "showmenu" : "admin-header-list"}>
-                <li>
-                    <div className="btn-close" onClick={handleCloseMenu}></div>
-                </li>
-                <li className='admin-header-list__element'>
-                    <Link href="/admin">Alla Event</Link>
-                </li>
-                <li className={`${pathname === '/admin/1/event' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
-                    <Link href="/admin/1/event">Redigera Eventet</Link>
-                </li>
-                <li className={`${pathname === '/admin/1/discounts' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
-                    <Link href="/admin/1/discounts">Rabatter</Link>
-                </li>
-                <li className={`${pathname === '/admin/1/sponsors' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
-                    <Link href="/admin/1/sponsors">Sponsorer</Link>
-                </li>
-            </ul>
-        </nav>
+        <header id="admin-header">
+            <nav className="admin-header-nav">
+                <ul className={showMenu ? "admin-header-list showmenu" : "admin-header-list"}>
+                    <li className='admin-header-list__element'>
+                        <Link href="/admin">Alla Event</Link>
+                    </li>
+                    <li className={`${pathname === '/admin/1/event' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
+                        <Link href="/admin/1/event">Redigera Eventet</Link>
+                    </li>
+                    <li className={`${pathname === '/admin/1/discounts' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
+                        <Link href="/admin/1/discounts">Rabatter</Link>
+                    </li>
+                    <li className={`${pathname === '/admin/1/sponsors' ? 'admin-header-list__element active' : 'admin-header-list__element'}`}>
+                        <Link href="/admin/1/sponsors">Sponsorer</Link>
+                    </li>
+                </ul>
+
+                <div id="hamburger" onClick={handleShowMenu}>
+                    <Image src={showMenu ? '/icon-x.svg' : '/icon-hamburger.svg'} width={20} height={20} />
+                </div>
+            </nav>
+        </header>
     );
 }
