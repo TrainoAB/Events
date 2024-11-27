ca5cb99ea7dd33cb7bf55a93e64e8d3fbe64f987
Mattias <mattiasjohansson.vfx@gmail.com>
2024-11-27 10:38:12 +0100

    Create a debug context to allow for debug console logs.

--- event/app/layout.jsx
+++ event/app/layout.jsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { DebugContextProvider } from "./context/DebugContext";
 import DevLogin from "./components/DevLogin";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +13,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
-            <body className={inter.className}>
-                <DevLogin>
-                    {children}
-                </DevLogin>
-            </body>
-        </html>
+        <DebugContextProvider>
+            <html lang="en">
+                <body className={inter.className}>
+                    <DevLogin>{children}</DevLogin>
+                </body>
+            </html>
+        </DebugContextProvider>
     );
 }
