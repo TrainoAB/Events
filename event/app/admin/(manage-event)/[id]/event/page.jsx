23524f408e8d00537fed244939a5a40bd5aa576d
joelBit <joel.rollny@gmail.com>
2024-11-27 13:40:45 +0100

    update event

--- event/app/admin/(manage-event)/[id]/event/page.jsx
+++ event/app/admin/(manage-event)/[id]/event/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 
 import "./page.css";
@@ -8,6 +8,10 @@ import "./page.css";
 export default function EditEventPage({ params }) {
     const router = useRouter();
     const [ event, setEvent ] = useState();
+    const eventRef = useRef();
+    const dateRef = useRef();
+    const imageRef = useRef();
+    const descriptionRef = useRef();
 
     useEffect(() => {
         fetchEvent();
@@ -22,7 +26,24 @@ export default function EditEventPage({ params }) {
     }
 
     const handleCancel = () => {
-        router.back();
+        router.push("/admin");
+    }
+
+    const handleSubmit = async () => {      // Is used instead of Server Action to keep test-data changes alive
+        await fetch('/api/event', {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({
+                'id': event.id,
+                'url': event.url,       // have input field for editing url?
+                'date': dateRef.current?.value,
+                'description': descriptionRef.current?.value,
+                'competition': eventRef.current?.value,
+                'image': imageRef.current?.value
+            })
+        });
     }
 
     return (
@@ -32,24 +53,24 @@ export default function EditEventPage({ params }) {
             { event ? <form className="edit-event-form flex-col">
                 <div className="input-wrapper">
                     <label htmlFor="event">Event</label>
-                    <input id="event" name="event" type="text" defaultValue={event.competition} required />
+                    <input id="event" name="event" type="text" ref={eventRef} defaultValue={event.competition} required />
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="date">Datum</label>
-                    <input id="date" name="date" type="date" required />
+                    <input id="date" name="date" type="date" ref={dateRef} required />
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="image">Bild (text länk)</label>
-                    <input id="image" name="image" type="text" defaultValue={event.image} required />
+                    <input id="image" name="image" type="text" ref={imageRef} defaultValue={event.image} required />
                 </div>
                 <div className="input-wrapper">
                     <label htmlFor="description">Beskrivning</label>
-                    <textarea id="description" name="description" rows={10} cols={100} defaultValue={event.description} required />
+                    <textarea id="description" name="description" rows={10} cols={100} ref={descriptionRef} defaultValue={event.description} required />
                 </div>
 
                 <div className="edit-event-form__buttons">
-                    <button onClick={handleCancel} type="reset">Avbryt</button>
-                    <button>Spara</button>
+                    <button type="reset" onClick={handleCancel}> Avbryt </button>
+                    <button type="submit" onClick={handleSubmit}> Spara </button>
                 </div>
             </form> : <></> }
         </main>
