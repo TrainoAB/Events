851f25229e7fb062a63931c94a6154595674e51f
joelBit <joel.rollny@gmail.com>
2024-11-28 16:06:03 +0100

    removed next_public_

--- event/db/db.js
+++ event/db/db.js
@@ -3,11 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const databaseClient = createClient(databaseURL(), databaseKey());
 
 function databaseURL() {
-    return process.env.NEXT_PUBLIC_DATABASE_URL ? process.env.NEXT_PUBLIC_DATABASE_URL : "";
+    return process.env.DATABASE_URL ? process.env.DATABASE_URL : "";
 }
 
 function databaseKey() {
-    return process.env.NEXT_PUBLIC_DATABASE_KEY ? process.env.NEXT_PUBLIC_DATABASE_KEY : "";
+    return process.env.DATABASE_KEY ? process.env.DATABASE_KEY : "";
 }
 
 export function getDatabaseClient() {
