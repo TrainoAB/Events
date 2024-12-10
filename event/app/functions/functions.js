7e121d9237580bf91f870cddc62aa5fc560ff549
Fredrik Berglund <fredrik@diam.se>
2024-12-10 15:18:25 +0100

    Fix

--- event/app/functions/functions.js
+++ event/app/functions/functions.js
@@ -1,4 +1,7 @@
+const DEBUG = process.env.NEXT_PUBLIC_DEBUG === "true";
+
 export function formatDate(dateString) {
+    DEBUG && console.log("Date string: ", dateString);
     const months = ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
     // Split dateString into year, month, and day
     const [year, month, day] = dateString.split("-").map(Number);
