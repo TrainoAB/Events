6a503f3285d8cfe40fe82502130888b5c9caf229
Mattias <mattiasjohansson.vfx@gmail.com>
2024-12-11 15:44:05 +0100

    Update WinnerCard with new css. Update event-finished page css.

--- event/app/components/WinnerCard.jsx
+++ event/app/components/WinnerCard.jsx
@@ -15,19 +15,15 @@ export default function WinnerCard({ placement, winner }) {
     };
 
     return (
-        <div id="winnerCard-wrapper">
-            <section id="winnerCard" className=" flex-col align-c">
-                <div className="winnerCard-placement-wrapper ">
-                    <div className={`winnerCard-placement ${winnerColor(placement)}`}>{placement}</div>
-                </div>
-                <div className="winnerCard-name-wrapper box-shadow flex-col">
-                    <h2>{winner.forename}</h2>
-                    <h2>{winner.surname}</h2>
-                </div>
-                <h2>{winner.city}</h2>
-                <h2>{ winner.result }</h2>
-                <h2 className="winnerCard-competition">{winner.competition}</h2>
-            </section>
-        </div>
+        <article id="winnerCard" className="flex-col align-c">
+            <div className={`winnerCard-placement ${winnerColor(placement)}`}></div>
+            <div className="winnerCard-name-wrapper">
+                <h2>{winner.forename}</h2>
+                <h2>{winner.surname}</h2>
+            </div>
+            <p className="winnerCard-city">{winner.city}</p>
+            <p>{ winner.result }</p>
+            <p className="winnerCard-competition">{winner.competition}</p>
+        </article>
     );
 }
