ea74f8a388e0898a10363a300a65b6a9f363f345
Fredrik Berglund <fredrik@diam.se>
2024-12-11 17:55:49 +0100

    Winners css updates

--- event/app/components/WinnerCard.jsx
+++ event/app/components/WinnerCard.jsx
@@ -15,14 +15,19 @@ export default function WinnerCard({ placement, winner }) {
     };
 
     return (
-        <article id="winnerCard" className="flex-col align-c">
+        <article id={`winnerCard${placement}`} className="winnerCard flex-col align-c">
+            <div className="glare">
+                <div className="glare__circle"></div>
+                <div className="glare__line"></div>
+                <div className="glare__line"></div>
+            </div>
             <div className={`winnerCard-placement ${winnerColor(placement)}`}></div>
             <div className="winnerCard-name-wrapper">
                 <h2>{winner.forename}</h2>
                 <h2>{winner.surname}</h2>
             </div>
             <p className="winnerCard-city">{winner.city}</p>
-            <p>{ winner.result }</p>
+            <p>{winner.result}</p>
             <p className="winnerCard-competition">{winner.competition}</p>
         </article>
     );
