0723bf745e0f751fc3247617d393b63f1535ec97
joelBit <joel.rollny@gmail.com>
2024-12-19 13:31:37 +0100

    Send confirmation email

--- event/app/actions/participant.js
+++ event/app/actions/participant.js
@@ -25,12 +25,29 @@ export async function createParticipant(id, _prevState, formData) {
     const { error } = await insertParticipant(participant);
     if (error) {
         console.log(error);
+
         if (error.code == '23505') {
             return { message: `${participant.email} är redan anmäld till tävlingen`, success: false };
         }
+
         return { message: "Du kunde inte registreras som deltagare", success: false };
     } else {
         console.log('Created participant ' + JSON.stringify(participant));
+
+        const response = await fetch('https://api.resend.com/emails', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env?.RESEND_API_KEY}`,
+            },
+            body: JSON.stringify({
+                from: 'Triathlon <events@traino.nu>',
+                to: `${participant.email}`,
+                subject: 'TRAINO Triathlon',
+                html: `<strong><h1>Tack för ditt engagemang!<h1></strong><br><br><h2>Du har blivit registrerad som deltagare för TRAINO Triathlon som går av stapeln 16:e Augusti 2025.</h2>`,
+            }),
+        });
+
         redirect('/triathlon/confirmation');
     }
 }
